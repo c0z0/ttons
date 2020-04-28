638b174159263fc945e7d6a20b1014f329a9dd44
Cosmin Serdean <cosmin@cserdean.com>
2020-04-28 22:21:49 +0300

    Add cache

--- packages/ttons/src/index.tsx
+++ packages/ttons/src/index.tsx
@@ -1,8 +1,10 @@
+import * as Icons from './icons';
+import * as breakPoints from './lib/breakPoints';
+
 export { Button } from './button';
 export { Container, ContentContainer } from './container';
-import * as Icons from './icons';
 export { Icons };
 export { Input } from './input';
 export { default as GlobalStyle } from './lib/GlobalStyle';
-export * as breakPoints from './lib/breakPoints';
-export {Text, CodeBlock, InlineCode} from './text';
+export { breakPoints };
+export { Text, CodeBlock, InlineCode } from './text';
