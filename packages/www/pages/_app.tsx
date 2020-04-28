950391800c38097a89dcdd824b1f0503d73fa7f7
Cosmin Serdean <cosmin@cserdean.com>
2020-04-28 22:41:25 +0300

    Optimize ssr

--- packages/www/pages/_app.tsx
+++ packages/www/pages/_app.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { CacheProvider } from '@emotion/core';
+
+import { cache } from 'emotion';
 
 import { GlobalStyle } from '@cserdean/ttons';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <CacheProvider value={cache}>
       <GlobalStyle theme="light" />
       <Component {...pageProps} />
-    </>
+    </CacheProvider>
   );
 }
